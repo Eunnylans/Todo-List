20158ba12ba53c34d0d453d55d41ff0c2b11d121
Eunice Abieyuwa Igbinedion <eunice.igbinedion@yahoo.com>
2023-05-17 21:40:26 +0200

    Updating Components

--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -1,23 +1,42 @@
 import { Injectable } from '@angular/core';
-import { TaskItem } from './task-item.dto';
-import { throwError } from 'rxjs';
 import { NewTask } from './new-task.dto';
+import { TaskItem } from './task-item.dto';
+import { Observable, BehaviorSubject, tap, map } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+const resourceURL = 'http://localhost:3001/tasks'
+
+@Injectable()
 export class TaskService {
-  addTask(date: Date, newTask: NewTask) {
-    throw new Error('Method not implemented.');
-  }
-  removeTask(date: Date, exisitingTask: TaskItem) {
-    throw new Error('Method not implemented.');
+
+  constructor(private httpClient: HttpClient) { }
+
+  private tasks = new BehaviorSubject<TaskItem[]>([])
+
+  getAllTasks(date: Date): Observable<TaskItem[]>{
+    this.httpClient.get<TaskItem[]>(`${resourceURL}/${date}`)
+      .pipe(map(TaskService.mapTaskItems))
+      .subscribe(t => this.tasks.next(t))
+
+      return this.tasks
   }
 
-  constructor() { }
-
-  getAllTasks(): TaskItem[]{
-    throw new Error('Not Implemented yet')
+  private static mapTaskItems(items: {title: string}[]){
+    return items.map(item => new TaskItem(item.title))
   }
 
+  addTask(date:Date, newTask: NewTask){
+    var updatedTasks = this.tasks.value.concat(new TaskItem(newTask.title))
+
+    this.httpClient.post(`${resourceURL}/${newTask.date}`, newTask)
+    .subscribe(() => this.tasks.next(updatedTasks))
+  }
+
+  removeTask(date: Date, existingTask: TaskItem){
+    var updatedTasks = this.tasks.value.filter(task => task != existingTask);
+
+    this.httpClient.delete(`${resourceURL}/${date}/${existingTask.title}`)
+      .subscribe(() => this.tasks.next(updatedTasks));
+
+  }
 }
